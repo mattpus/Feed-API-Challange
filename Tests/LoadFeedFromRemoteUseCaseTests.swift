02a8358f816325780273a75066bf63448329ef68
mateusz.pusiewicz <mateusz.pusiewicz@ing.com>
2023-04-23 08:21:51 +0200

    Do not deliver the result when the RemoteFeedLoader is dealocated

--- Tests/LoadFeedFromRemoteUseCaseTests.swift
+++ Tests/LoadFeedFromRemoteUseCaseTests.swift
@@ -116,17 +116,17 @@ class LoadFeedFromRemoteUseCaseTests: XCTestCase {
 	}
 
 	func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
-//		let url = URL(string: "http://any-url.com")!
-//		let client = HTTPClientSpy()
-//		var sut: RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
-//
-//		var capturedResults = [RemoteFeedLoader.Result]()
-//		sut?.load { capturedResults.append($0) }
-//
-//		sut = nil
-//		client.complete(withStatusCode: 200, data: makeItemsJSON([]))
-//
-//		XCTAssertTrue(capturedResults.isEmpty)
+		let url = URL(string: "http://any-url.com")!
+		let client = HTTPClientSpy()
+		var sut: RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
+
+		var capturedResults = [RemoteFeedLoader.Result]()
+		sut?.load { capturedResults.append($0) }
+
+		sut = nil
+		client.complete(withStatusCode: 200, data: makeItemsJSON([]))
+
+		XCTAssertTrue(capturedResults.isEmpty)
 	}
 
 	// MARK: - Helpers
